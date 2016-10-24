cacd58176beec4af7d698de549342f3aa216d0c2
Sophia Liu <sliu3@wellesley.edu>
2016-10-24 12:37:11 -0700

    Enums

---
+++ jav_enums.js
@@ -0,0 +1,51 @@
+"use strict"
+
+// double_array
+
+Array.prototype.doubleArray = function(){
+  const doubleArray = [];
+  this.forEach(function(el){
+    double_array.push(el*2)
+  });
+  return double_array;
+}
+//
+// [2,4,5].double_array();
+
+const NUMS = [2,3,4];
+
+// myEach
+Array.prototype.myEach = function(func){
+  for(let i = 0; i < this.length; i++){
+    func(this[i]);
+  }
+  return this;
+}
+
+// NUMS.myEach((num) => {
+//   console.log(`Someone's favorite number is ${num}`);
+// });
+
+
+//
+// myMap
+// => It must use your myEach function.
+// => Use a closure.
+
+
+Array.prototype.myMap = function(func){
+  const newArray = [];
+  this.myEach(el => newArray.push(func(el)));
+  return newArray;
+}
+
+NUMS.myMap((num) => {
+  console.log(num*2);
+});
+
+// myInject
+// Your inject should take a function.
+// As the exercise describes, start the accumulator variable with the first value. Iterate through the rest.
+// It must also use your myEach function.
+//
+// Array.prototype.my_inject = function()
